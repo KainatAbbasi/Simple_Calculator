7122d5f1a31831ea24afb2aeb376527d99ea9fb0
Kainat Abbasi <KainatAkmal2003@gmail.com>
2024-03-23 03:46:58 -0700

    Updated Calculator🌸

--- index.js
+++ index.js
@@ -1,37 +1,74 @@
 #! /usr/bin/env node
 // =============== Calculator ====================//
+console.log("Welcome To Simple CLI Calculator :");
 import inquirer from 'inquirer';
-const answer = await inquirer.prompt([
+const permission = await inquirer.prompt([
     {
-        type: 'number',
-        name: 'firstNumber',
-        message: 'Enter First Number :',
-    },
-    {
-        type: 'number',
-        name: 'secondNumber',
-        message: 'Enter second Number :',
-    },
-    {
-        type: 'list',
-        name: 'operator',
-        message: 'Select your desired opration',
-        choices: ['Addition', 'Subtraction', 'Multiplication', 'Division']
+        type: 'string',
+        name: 'password',
+        message: 'Enter Password :',
     },
 ]);
-// conditional statement;
-if (answer.operator === 'Addition') {
-    console.log(answer.firstNumber + answer.secondNumber);
-}
-else if (answer.operator === 'Subtraction') {
-    console.log(answer.firstNumber - answer.secondNumber);
-}
-else if (answer.operator === 'Multiplication') {
-    console.log(answer.firstNumber * answer.secondNumber);
-}
-else if (answer.operator === 'Division') {
-    console.log(answer.firstNumber / answer.secondNumber);
+let pass = 'fly$';
+if (permission.password === pass) {
+    while (true) {
+        const answer = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'operator',
+                message: 'Select your desired opration',
+                choices: ['Addition', 'Subtraction', 'Multiplication', 'Division']
+            },
+            {
+                type: 'number',
+                name: 'firstNumber',
+                message: 'Enter First Number :',
+            },
+            {
+                type: 'number',
+                name: 'secondNumber',
+                message: 'Enter second Number :',
+            },
+        ]);
+        // conditional statement;
+        if (answer.operator === 'Addition') {
+            console.log("The Addition of ", answer.firstNumber, "and", answer.secondNumber, "is :", answer.firstNumber + answer.secondNumber);
+        }
+        else if (answer.operator === 'Subtraction') {
+            console.log("The Subtraction of ", answer.firstNumber, "and", answer.secondNumber, "is :", answer.firstNumber - answer.secondNumber);
+        }
+        else if (answer.operator === 'Multiplication') {
+            console.log("The Multiplication of ", answer.firstNumber, "and", answer.secondNumber, "is :", answer.firstNumber * answer.secondNumber);
+        }
+        else if (answer.operator === 'Division') {
+            if (answer.secondNumber === 0) {
+                console.log('Cannot divide by zero');
+            }
+            else {
+                console.log("The Division of ", answer.firstNumber, "and", answer.secondNumber, "is :", answer.firstNumber / answer.secondNumber);
+            }
+        }
+        else {
+            console.log('Invalid Operator');
+        }
+        ;
+        const ask = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'response',
+                message: 'Do you want to Continue or Exit ?',
+                choices: ['Continue', 'Exit']
+            }
+        ]);
+        if (ask.response === 'Exit') {
+            console.log('Goodbye!');
+            break;
+        }
+        else {
+            continue;
+        }
+    }
 }
 else {
-    console.log('Invalid Operator');
+    console.log('Wrong password ! Try again ');
 }

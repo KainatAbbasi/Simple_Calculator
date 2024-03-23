431c25d0b9154cd24066c1f85f344cb32f5e673a
Kainat Abbasi <KainatAkmal2003@gmail.com>
2024-03-23 03:15:43 -0700

    Set a password for calculator

---
+++ README.md
@@ -0,0 +1,2 @@
+# Typescript_Mini_Projects
+Welcome to my Typescript Mini Project repository ! 🌸Here, you'll find a collection of small yet impactful Typescript project that demonstrate my coding journey and proficiency with this versatile language.
---
+++ index.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+// =============== Calculator ====================//
+
+import inquirer from 'inquirer'
+const answer = await inquirer.prompt([
+    {
+        type: 'number',
+        name: 'firstNumber',
+        message: 'Enter First Number :',
+    },
+    {
+        type: 'number',
+        name: 'secondNumber',
+        message: 'Enter second Number :',
+    },
+    {
+        type: 'list',
+        name: 'operator',
+        message: 'Select your desired opration',
+        choices: ['Addition', 'Subtraction', 'Multiplication', 'Division']
+    },
+]);
+
+// conditional statement;
+
+if(answer.operator === 'Addition'){
+    console.log(answer.firstNumber + answer.secondNumber); 
+}else if(answer.operator === 'Subtraction'){
+    console.log(answer.firstNumber - answer.secondNumber); 
+}else if(answer.operator === 'Multiplication'){
+    console.log(answer.firstNumber * answer.secondNumber); 
+}else if(answer.operator === 'Division'){
+    console.log(answer.firstNumber / answer.secondNumber); 
+}else{
+    console.log('Invalid Operator');
+}
